92528027e39c79022140efacdf4d408493f2835d
arisupriatna14 <arisupriatna703@gmail.com>
2018-09-21 14:19:16 +0700

    add aws s3, and aws recognition

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const busboy = require('connect-busboy')
+const busboyBodyParser = require('busboy-body-parser')
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const awsRouter = require('./routes/aws')
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors())
+app.use(busboy())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(busboyBodyParser())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/aws', awsRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
