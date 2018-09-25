7bdb6761699078e107e2999b79be745df78c888a
arisupriatna14 <arisupriatna703@gmail.com>
2018-09-25 17:34:59 +0700

    deleted helpers socker client

--- cron/scheduleOnHour.js
+++ cron/scheduleOnHour.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose')
 const Schedule = require('../models/schedule')
 const User = require('../models/users')
 const Prescription = require('../models/prescription')
-const io = require('../helpers/socketClient')
+// const io = require('../helpers/socketClient')
 module.exports = () => {
  console.log("cron on triger")
 
  new CronJob(`0 */1 * * * *`, function() {
-    io.emit('message', 'from API ')
+    // io.emit('message', 'from API ')
     console.log('testtttttt')
     let MONGO_URI = {
         development:`mongodb://${process.env.dbProdAdm}:${process.env.dbProdAdm}@ds259912.mlab.com:59912/susidb`,
