66fbde04e8f003b6b9ebcaaf15b912ce3ea9240e
Grayfield <grayfieldmm@gmail.com>
2018-09-25 18:13:59 +0700

    create prescription by recognition result

--- controllers/recognition.js
+++ controllers/recognition.js
@@ -1,15 +1,17 @@
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
+
+const { createPrescription } = require('./prescription');
 
 const rekognition = new AWS.Rekognition({
-  apiVersion: "2016-06-27",
+  apiVersion: '2016-06-27',
   region: process.env.REGION,
   accessKeyId: process.env.IAM_USER_KEY,
-  secretAccessKey: process.env.IAM_USER_SECRET,
+  secretAccessKey: process.env.IAM_USER_SECRET
 });
 
 module.exports = {
   textRecognition: (req, res) => {
-    const { key, VersionId } = req.dataUpload
+    const { key, VersionId } = req.dataUpload;
     let params = {
       Image: {
         S3Object: {
@@ -22,30 +24,36 @@ module.exports = {
 
     rekognition.detectText(params, (err, data) => {
       if (err) {
-        res
-          .status(500)
-          .json({
-            errMessage: err
-          })
+        res.status(500).json({
+          errMessage: err
+        });
       } else {
-        let arrResult = []
-        let hasilAkhir = []
+        let arrResult = [];
+        let hasilAkhir = [];
         data.TextDetections.map(obat => {
-          arrResult.push(obat.DetectedText)
-        })
+          arrResult.push(obat.DetectedText);
+        });
 
         arrResult.splice(1, 5).map(obatSplit => {
-          hasilAkhir.push(obatSplit.split(': ')[1])
-        })
+          hasilAkhir.push(obatSplit.split(': ')[1]);
+        });
 
-        res.status(200).json({
-          info: "Data detect text success",
-          data: hasilAkhir
-        })
+        console.log('<============== hasil recognition ========> ');
+        console.log(hasilAkhir);
 
-        console.log('<============== hasil recognition ========> ')
-        console.log(hasilAkhir)
+        const data = {
+          userId: req.userId,
+          label: hasilAkhir[0],
+          route: hasilAkhir[1],
+          expDate: hasilAkhir[2],
+          times: hasilAkhir[3],
+          stock: hasilAkhir[4]
+        };
+
+        const prescriptionSchedule = createPrescription(data, null);
+
+        res.status(200).json(prescriptionSchedule);
       }
     });
   }
-}
+};
