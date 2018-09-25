edebe4d8fd16a22115761842265c636f7220585b
Grayfield <grayfieldmm@gmail.com>
2018-09-25 16:02:15 +0700

    add socket client

--- cron/scheduleOnHour.js
+++ cron/scheduleOnHour.js
@@ -1,5 +1,6 @@
-require('dotenv').config()
+require('dotenv').config();
 const CronJob = require('cron').CronJob;
+<<<<<<< HEAD
 const mongoose = require('mongoose')
 const Schedule = require('../models/schedule')
 const User = require('../models/users')
@@ -8,42 +9,82 @@ const Prescription = require('../models/prescription')
 //helpers
 // const routeMedicine = require('../helpers/route')
 // const io = require('../helpers/socketClient')
+=======
+const mongoose = require('mongoose');
+const Schedule = require('../models/schedule');
+const User = require('../models/users');
+const Prescription = require('../models/prescription');
+
+const io = require('../helpers/socketClient');
+
+>>>>>>> add socket client
 module.exports = () => {
- console.log("cron on triger")
+  console.log('cron on triger');
 
- new CronJob(`0 */1 * * * *`, function() {
-    // io.emit('message', 'from API ')
-    console.log('testtttttt')
-    let MONGO_URI = {
-        development:`mongodb://${process.env.dbProdAdm}:${process.env.dbProdAdm}@ds259912.mlab.com:59912/susidb`,
-        test:`mongodb://${process.env.dbTestAdm}:${process.env.dbTestAdm}@ds259912.mlab.com:59912/susidbtest`
-      }
-      mongoose.connect(MONGO_URI[process.env.NODE_ENV], { useNewUrlParser: true, useCreateIndex: true } ,async function(err){
-            if(err) console.log("connect with mLab on error : ", err)
-            try{
-                console.log("===============================================")
-                    let scheduleOnDb = await Schedule.find({
-                            time:{
-                                $gt : (new Date()).setSeconds(-60),
-                                $lte : (new Date()).setSeconds(0)
-                            },
-                        }).populate('userId').populate('prescriptionId').exec()
+  new CronJob(
+    `0 */1 * * * *`,
+    function() {
+      let MONGO_URI = {
+        development: `mongodb://${process.env.dbProdAdm}:${
+          process.env.dbProdAdm
+        }@ds259912.mlab.com:59912/susidb`,
+        test: `mongodb://${process.env.dbTestAdm}:${
+          process.env.dbTestAdm
+        }@ds259912.mlab.com:59912/susidbtest`
+      };
+      mongoose.connect(
+        MONGO_URI[process.env.NODE_ENV],
+        { useNewUrlParser: true, useCreateIndex: true },
+        async function(err) {
+          if (err) console.log('connect with mLab on error : ', err);
+          try {
+            console.log('===============================================');
+            let now = new Date();
+            now.setSeconds(0);
+            let nextMinute = new Date(now.getTime() + 60000);
+            nextMinute.setSeconds(0);
 
-                    console.log("--------------------------------------------------") 
-                    console.log("time on tick on minute : ", new Date(new Date().setSeconds(0)).toLocaleString(), "; range :", new Date( new Date().setSeconds(60) ).toLocaleString() )
-                    console.log("--------------------------------------------------")
-                    scheduleOnDb.forEach(async e => {
-                        console.log(`${e.onSchedule} ini ${e.userId.name} saatnya minum obat : ${e.prescriptionId.label}, stock : ${e.prescriptionId.stock}`)
-                        let message = `${e.onSchedule} ini ${e.userId.name} saatnya minum obat : ${e.prescriptionId.label}, stock : ${e.prescriptionId.stock}`
-                        // io.emit('message', message )
-                    })
-                    
+            let scheduleOnDb = await Schedule.find({
+              time: {
+                $gte: now,
+                $lt: nextMinute
+              }
+            })
+              .populate('userId')
+              .populate('prescriptionId')
+              .exec();
 
-                    console.log("===============================================")          
-            } catch (error) {
-                console.log(error)
-            }
-        })
-        
-      }, null, true, 'Asia/Jakarta');
-}
+            console.log(
+              'time on tick on minute : ',
+              now.toLocaleString(),
+              '; range :',
+              nextMinute.toLocaleString()
+            );
+            console.log('--------------------------------------------------');
+            scheduleOnDb.forEach(e => {
+              console.log(
+                `${e.onSchedule} ini ${e.userId.name} saatnya minum obat : ${
+                  e.prescriptionId.label
+                }, stock : ${e.prescriptionId.stock}`
+              );
+              io.emit(
+                'notification',
+                `${e.onSchedule}, halo ${e.userId.name} saatnya minum obat : ${
+                  e.prescriptionId.label
+                }, stock : ${e.prescriptionId.stock}`
+              );
+            });
+            console.log('--------------------------------------------------');
+
+            console.log('===============================================');
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      );
+    },
+    null,
+    true,
+    'Asia/Jakarta'
+  );
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+<<<<<<< HEAD
+=======
+
+const io = require('socket.io-client')(process.env.socketUrl);
+
+>>>>>>> add socket client
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200).json({ message: 'Connected to Susi API' });
